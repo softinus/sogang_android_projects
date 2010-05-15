5b7bf97062b02f81d048884a77f36f699aafb12b
thks10 <thks10@6b6743b8-8582-0410-8b59-e13458315b7f>
2010-05-15 02:48:28 +0000

    스테이지1::최종리비전, StageManager Refactoring전 백업

--- RealSunMoon/src/com/raimsoft/view/GameView.java
+++ RealSunMoon/src/com/raimsoft/view/GameView.java
@@ -8,9 +8,13 @@ package com.raimsoft.view;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.PowerManager;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -18,15 +22,16 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 import com.raimsoft.activity.GameActivity;
 import com.raimsoft.activity.R;
-import com.raimsoft.game.RenderObject;
-import com.raimsoft.game.StageManager;
-import com.raimsoft.object.Monster;
-import com.raimsoft.object.Player;
-import com.raimsoft.object.Rope;
-import com.raimsoft.object.TreadleManager;
+import com.raimsoft.game.Monster;
+import com.raimsoft.game.Player;
+import com.raimsoft.game.Rope;
+import com.raimsoft.game.TreadleManager;
 import com.raimsoft.sensor.SensorFactory;
 import com.raimsoft.sound.SoundManager;
 
@@ -34,7 +39,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 {
 	public GameThread thread;
 	
-	
+	SensorFactory sf= SensorFactory.getSensorFactory();
 	
 	PowerManager pm;
 	PowerManager.WakeLock wl;
@@ -53,7 +58,10 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 		gameContext= (GameActivity) context;
 		this.thread.view= this;
 		
-		
+		this.thread.mPlayer= new Player(this, 150,430, 45,50, R.drawable.nui_jump_left);
+		this.thread.treadleMgr= new TreadleManager(this);
+		this.thread.mMonster= new Monster(this, -1, -1 ,50,45, R.drawable.bird_fly_1);
+		this.thread.mRope= new Rope(this, 140,-2, 17,168, R.drawable.new_rope);
 		
 		pm= (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 		wl= pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "My Tag");
@@ -68,12 +76,24 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 		private SurfaceHolder mSurfaceHolder;// 화면 제어
 		private boolean bRun=true;			// 동작 여부
 		private boolean bMove=true;			// 움직임 여부 (일시정지)
+		private boolean bGameClear=false;	// 게임 클리어 여부
+		
+		private boolean bPlayer_ImgRefreshed=true;	// 이미지 새로고침(플레이어)
+		private boolean bTreadle_ImgRefreshed=true;	// 이미지 새로고침(발판)
+		private boolean bMonster_ImgRefreshed=true;	// 이미지 새로고침(몬스터)
+		private boolean bRope_ImgRefreshed=true;	// 이미지 새로고침(로프)
 
+		public Player mPlayer;				// 플레이어 객체
+		public TreadleManager treadleMgr;	// 발판 객체
+		public Monster mMonster;			// 몬스터 객체
+		public Rope mRope;
+		
 		private Resources mRes;				// 리소스
+		private Paint pDebug=new Paint();	// 페인트
+		private Paint pScore=new Paint();
 
-		//private Drawable dGameClear;
-
-		public int cnt_Step=0;				// 발판 밟은 수 
+		private Bitmap bBackground;			// 배경
+		private Drawable dGameClear;
 		
 		private int AccFrame;				// 누적프레임
 		private int FPS;					// 초당 프레임
@@ -82,13 +102,15 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 		private long curTime, oldTime;		// 현재시간, 지난시간
 		private int delTime=5;				// Thread딜레이
 		
-		private RenderObject renderObj;
-
+		public int BackSize=1920;			// 배경세로길이
+		public int cnt_Step=0;				// 발판 밟은 수 
+		public int gameScore=0;
+		private int clearTranspercy=0;
 		
 		Canvas canvas=null;
 		//Bundle SaveBox=new Bundle();
 		
-		StageManager stageMgr;
+		
 		SoundManager sm;
 				
 		// 메인스레드의 생성자
@@ -97,18 +119,22 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 			mSurfaceHolder= _Holder;
 			mRes= _Context.getResources();
 			
-			renderObj= new RenderObject(mRes, _Context);
+			pDebug.setTextSize(12);
+			pDebug.setAntiAlias(true);
+			pDebug.setColor(Color.argb(0xff, 255, 0, 255));
 			
-			
-			stageMgr=new StageManager();
-			
-			stageMgr.ChangeStage(1);
+			pScore.setTextSize(24);
+			pScore.setAntiAlias(true);
+			pScore.setColor(Color.argb(0xff, 255, 0, 255));
 			
 //			sm=new SoundManager(_Context);
 //			sm.create();
 //			sm.load(0, R.raw.game_bgm);
 //			sm.play(0);
 			
+			
+			
+			bBackground= BitmapFactory.decodeResource(mRes, R.drawable.background_1);
 		}
 		
 
@@ -125,9 +151,11 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 						oldTime= System.currentTimeMillis();
 						canvas.save();
 						
-						stageMgr.Render(canvas);
-						
-						if (bMove)	stageMgr.FrameMove();
+						doDrawBackGround(canvas);
+						doDrawObject(canvas);
+						//doDrawText(canvas);
+						doDrawScore(canvas);
+						if (bMove)	doMove();
 						
 						canvas.restore();
 						
@@ -146,7 +174,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 							RenderAccTime=0;
 						}
 						
-						stageMgr.LikeCheck();
+						this.LifeCheck();
 					}
 				}catch (InterruptedException e) {
 					e.printStackTrace();
@@ -160,11 +188,185 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 			}
 		}
 		
+		void doDrawBackGround(Canvas c)
+		{			
+			c.drawBitmap(bBackground,
+			new Rect(0, BackSize-view.getHeight(),view.getWidth(),BackSize)
+			, new Rect(0,0,view.getWidth(),view.getHeight()), null);
+		}
 		
+		void doDrawText(Canvas c)
+		{
+			c.drawText("BackSize= "+ Float.toString(BackSize), 5, 45, pDebug);
+
+//			c.drawText("FPS= " + Float.toString(FPS), 5, 30, pDebug);
+//			c.drawText("Frame= " + Float.toString(AccFrame), 5, 15, pDebug);
+//			c.drawText("X= "+Float.toString(mPlayer.getX()) 
+//						+ ", Y= "+Float.toString(mPlayer.getY()),
+//						mPlayer.getX(), mPlayer.getY(), pDebug);
+//			c.drawText("State= " + Float.toString(mPlayer.State), 5, 45, pDebug);
+//			
+//			c.drawText("X= " + Float.toString(sf.getSensorValue()[0])
+//						+"Y= "+ Float.toString(sf.getSensorValue()[1])
+//						+"Z= "+ Float.toString(sf.getSensorValue()[2])
+//						, 5, 60, pDebug);
+			
+			//for (int i=0; i<treadleMgr.getCount(); i++)
+			//{
+//			c.drawText("X= "+ Float.toString(treadleMgr.treadle[i].getX())
+//						+", Y= "+ Float.toString(treadleMgr.treadle[i].getY()),
+//						treadleMgr.treadle[i].getX(), treadleMgr.treadle[i].getY(), p);
+			//}
+		}
+		
+		void doDrawScore(Canvas c)
+		{
+			c.drawText("점수 : " + Float.toString(gameScore), 5, 20, pScore);
+		}
 		
 
 		
-		/** 캐릭터 동작 설정
+		
+		void doDrawObject(Canvas c)
+		{
+			
+			
+			if(bRope_ImgRefreshed)
+			{
+				mRope.Img_Drawable= mRes.getDrawable(mRope.Img_id);
+				bRope_ImgRefreshed=false;
+			}
+			
+			if(bPlayer_ImgRefreshed)
+			{
+				//Log.v("verbose", "Call Player_ImgRefreshed");
+				mPlayer.Img_Drawable= mRes.getDrawable(mPlayer.Img_id);
+				bPlayer_ImgRefreshed= false;
+			}
+			
+			if(bTreadle_ImgRefreshed)
+			{
+				for (int i=0; i<treadleMgr.getCount(); i++)
+				{
+					
+					Log.v("Draw Treadle", i + "th treadle Loading");
+					treadleMgr.treadle[i].Img_Drawable= mRes.getDrawable(treadleMgr.treadle[i].Img_id);	
+				}
+				bTreadle_ImgRefreshed=false;
+			}
+			
+			if(bMonster_ImgRefreshed)
+			{
+				mMonster.Img_Drawable= mRes.getDrawable(mMonster.Img_id);
+				bMonster_ImgRefreshed=false;
+			}
+			
+			
+			
+			
+			for (int i=0; i<treadleMgr.getCount(); i++)
+			{
+				//Log.v("Draw Treadle", i + "th treadle Drawing");
+				if (IsNotClipped(mPlayer.getY(), treadleMgr.treadle[i].getY())) {
+					treadleMgr.treadle[i].Img_Drawable.setBounds(treadleMgr.treadle[i].getObjectForRect());
+					treadleMgr.treadle[i].Img_Drawable.draw(c);				
+				}
+			}
+			
+			if (IsNotClipped(mPlayer.getY(), mMonster.getY()))
+			{
+				mMonster.Img_Drawable.setBounds(mMonster.getObjectForRect());
+				mMonster.Img_Drawable.draw(c);
+			}
+			
+			if (bGameClear)	//클리어시에 로프 그림
+			{
+				mRope.Img_Drawable.setBounds(mRope.getObjectForRect());
+				mRope.Img_Drawable.draw(c);
+			}
+			
+			mPlayer.Img_Drawable.setBounds(mPlayer.getObjectForRect());
+			mPlayer.Img_Drawable.draw(c);
+			
+			if (bGameClear)
+			{
+				mRope.setX(mPlayer.getX()+(mPlayer.getWidth()/2));
+				if(mRope.getY() != -5)
+				{
+					mRope.RopeDown();
+				}else{
+					mRope.bRopeDown=false;
+				}
+				
+				if (!mRope.bRopeDown)
+				{
+					canvas.drawARGB(clearTranspercy, 0, 0, 0);
+					if (clearTranspercy<255)
+					{
+						clearTranspercy+=5;
+					}
+				}
+				
+			}
+			
+//			if (!mRope.bRopeDown)
+//			{
+//				dGameClear= mRes.getDrawable(R.drawable.game_clear);
+//				dGameClear.setBounds(20,200, 261+20,60+200);
+//				dGameClear.draw(c);
+//			}
+		}
+		
+		public void doMove()
+		{
+			if (mPlayer.bCrushed)
+			{
+				mPlayer.CrushFall();
+			}else{
+				mPlayer.SensorMove( sf.getCompressFloat2X( sf.getSensorValue() ) );
+				mPlayer.JumpAlways();
+			}
+			
+			mMonster.Move_Bird();
+			treadleMgr.ALL_Treadle_Stepped();
+			
+			mPlayer.CollisionMonster(mMonster.getObjectForRect());
+			
+			for (int i=0; i<treadleMgr.getCount(); i++)
+			{
+				if (mPlayer.bJump==true)
+				{
+					mPlayer.CollisionTreadle(treadleMgr.treadle[i].getObjectForRectHalf(true)
+							, treadleMgr.treadle[i],	treadleMgr);
+				}
+				
+			}
+			
+		}
+		
+		
+		/**
+		 * 캐릭터와 발판들의 거리를 계산하여 화면 밖에 있는 객체들은 DRAW하지 않습니다.
+		 * @param player_Y : 캐릭터의 Y값
+		 * @param Object_Y : 체크할 다른 객체의 Y값
+		 * @return 화면안에 있으면:true, 화면밖에 있으면:false
+		 */
+		boolean IsNotClipped(int player_Y, int Object_Y)
+		{
+			if (Math.abs(Object_Y - player_Y) > view.getHeight())
+				return false;
+			return true;
+		}
+		
+		void LifeCheck()
+		{
+			if (!mPlayer.isLive())
+			{
+				gameContext.NextGameOverActivity();
+			}
+		}
+		
+		/** 스레드 동작 설정
 		* @param _Run : 동작 설정 boolean값*/
 		public void setRunning(boolean _Run)
 		{
@@ -179,7 +381,19 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 			bMove= _Move;
 		}
 		
-
+		public void setGameClear(boolean _Clear)
+		{
+			bGameClear= _Clear;
+		}
+		
+		public void setPlayerImg_Refresh()
+		{
+			bPlayer_ImgRefreshed= true;
+		}
+		public void setTreadleImg_Refresh()
+		{
+			bTreadle_ImgRefreshed= true;
+		}
 	}
 		
 
