ab39d916b56b27180482e3f41dcff5b4986a4565
thks10 <softinus@users.noreply.github.com>
2010-07-05 11:38:06 +0000

    SpriteBitmap Class realize

---
+++ MatanShooter/src/com/raimsoft/util/FrameManager.java
@@ -0,0 +1,23 @@
+package com.raimsoft.util;
+
+
+public class FrameManager
+{
+	private static FrameManager fm= new FrameManager();	// 싱글톤
+
+	public int TotalFrame=0;
+
+
+
+
+	public static FrameManager getInstance()
+	{
+		return fm;
+	}
+	public void IncreaseTotalFrame()
+	{
+		++TotalFrame;
+		//Log.d("FrameManager", Float.toString(TotalFrame));
+	}
+
+}
--- MatanShooter/src/com/raimsoft/util/SpriteBitmap.java
+++ MatanShooter/src/com/raimsoft/util/SpriteBitmap.java
@@ -1,5 +1,77 @@
 package com.raimsoft.util;
 
-public class SpriteBitmap {
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.Log;
 
+public class SpriteBitmap
+{
+	Bitmap mBitmap;
+	Rect CropRect, DestRect;
+	FrameManager mFrameMgr= FrameManager.getInstance();
+	int width, height;
+	int count, SpriteNum;
+	int Delay, TotalFrame;
+
+	public SpriteBitmap(int ResourceID, Resources _res, int _wid, int _hei,
+						int _SpriteNum, int _Delay)
+	{
+		mBitmap= BitmapFactory.decodeResource(_res, ResourceID);
+		width= _wid;
+		height= _hei;
+		SpriteNum= _SpriteNum;
+		Delay= _Delay;
+		count= 0;
+
+		CropRect= new Rect();
+		DestRect= new Rect();
+	}
+
+
+	public synchronized void Animate (Canvas canvas, int DestinationX, int DestinationY)
+	{
+		if (TotalFramePerDelay())
+		{
+			++count;
+		}
+
+		RectSetting(DestinationX, DestinationY);
+		canvas.drawBitmap(mBitmap, CropRect, DestRect, null);
+
+		if (count == SpriteNum) count=0; //스프라이트 반복
+	}
+
+	private void RectSetting(int _destX, int _destY)
+	{
+		CropRect.left	= width*count;
+		CropRect.top	= 0;
+		CropRect.right	= width*count+width;
+		CropRect.bottom	= height;
+
+		DestRect.left	= _destX;
+		DestRect.top	= _destY;
+		DestRect.right	= _destX+width;
+		DestRect.bottom	= _destY+height;
+	}
+
+	private boolean TotalFramePerDelay ()
+	{
+		TotalFrame= mFrameMgr.TotalFrame;
+
+		if (TotalFrame % Delay == 0)
+		{
+			return true;
+		}
+		return false;
+	}
+
+
+	public void DeleteSprite()
+	{
+		mBitmap=null;
+		CropRect=null;
+	}
 }
