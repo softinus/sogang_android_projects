8e86a4963cfab004e9f1ccada9dc022e5e8295ae
thks10 <softinus@users.noreply.github.com>
2010-07-27 09:01:47 +0000

    Zombies route design, Motion vector normalize method completed.

--- MatanShooter/src/com/raimsoft/matan/object/Zombie.java
+++ MatanShooter/src/com/raimsoft/matan/object/Zombie.java
@@ -2,7 +2,6 @@ package com.raimsoft.matan.object;
 
 import android.content.res.Resources;
 
-import com.raimsoft.matan.motion.MotionVectors;
 import com.raimsoft.matan.motion.Moveable;
 import com.raimsoft.matan.util.FPoint;
 import com.raimsoft.matan.util.SpriteBitmap;
@@ -11,32 +10,38 @@ import com.raimsoft.matan.util.Vector2Calc;
 public class Zombie extends GameObject implements Moveable
 {
 	public SpriteBitmap SPRITEwalk;
-	private boolean bPositioning= false;
+	//private boolean bPositioning= false;
 
-	MotionVectors motion;
+	//MotionVectors motion;
+
 	private FPoint vMove;
+	private FPoint vStart, vStop;
 
 
-	public Zombie(float X, float Y, int IDimage, int Width, int Height, Resources mRes)
+	public Zombie(float X, float Y,  FPoint endPoint, int IDimage, int Width, int Height, Resources mRes)
 	{
 		super(X, Y, IDimage, Width, Height);
 
 		SPRITEwalk= new SpriteBitmap(IDimage, mRes, 100,100, 4, 10);
-		motion= new MotionVectors();
 		vMove= new FPoint();
+
+		vStart= new FPoint(X,Y);
+
+		vStop= new FPoint();
+		vStop= endPoint;
 	}
 
 
 	@Override
-	public void Move(float speed, int MovePattenNum)
+	public void Move(float speed)
 	{
-		if (!bPositioning)
+		//if (!bPositioning)
 		{
-			vMove= Vector2Calc.CalVec(motion.pSrc[MovePattenNum], motion.pDes);
+			vMove= Vector2Calc.CalVec(vStart, vStop);
 			vMove= Vector2Calc.CalVecNormalize(vMove);
 			vMove= Vector2Calc.CalScale(vMove, speed);
 
-			bPositioning= true;
+			//bPositioning= true;
 		}
 
 		this.x+= vMove.x;
