969852afbfd080f00eb4da6f9993caf7e17d081e
thks10 <softinus@users.noreply.github.com>
2010-08-16 10:28:34 +0000

    Logo added.

---
+++ MatanShooter/src/com/raimsoft/matan/object/ZombieList.java
@@ -0,0 +1,13 @@
+package com.raimsoft.matan.object;
+
+import java.util.ArrayList;
+
+public class ZombieList
+{
+	ArrayList<Zombie> mZombieList;
+
+	public ZombieList()
+	{
+		mZombieList= new ArrayList<Zombie>();
+	}
+}
